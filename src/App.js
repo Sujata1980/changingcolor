aa35a2c110129c6ff7769a13a8a2717974d37c49
Sujata1980 <sujata.katoch@gmail.com>
2025-04-05 13:36:36 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [text,setText] = useState(false);
+
+ const handleClick = () =>{
+    setText(!text)
+    }
+  
+
+  
   return (
     <div className="App">
-      
+
       <h1>Changes Color</h1>
-      <h1>SUJATA AGARWAL</h1>
-      <button>ChangeColor</button>
+      <h1 style = {{ color : text ? "red" : ""}}>SUJATA AGARWAL</h1>
+      <button onClick={handleClick}>ChangeColor</button>
       
     </div>
   );
