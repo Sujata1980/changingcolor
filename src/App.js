1b96a15e5ce33475bb283049a70b65a16592e24e
Sujata1980 <sujata.katoch@gmail.com>
2025-04-06 12:57:39 +0530

    changes in app.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,10 @@ import './App.css';
 
 function App() {
 
-  const [text,setText] = useState(false);
+  const [color,changeColor] = useState("darkorange");
 
- const handleClick = () =>{
-    setText(!text)
-    }
+  
+ 
   
 
   
@@ -16,8 +15,14 @@ function App() {
     <div className="App">
 
       <h1>Changes Color</h1>
-      <h1 style = {{ color : text ? "red" : ""}}>SUJATA AGARWAL</h1>
-      <button onClick={handleClick}>ChangeColor</button>
+      {/* <h1 style = {{ color : text ? "red" : " "}}>SUJATA AGARWAL</h1> */}
+
+      <h1 style = {{color}}>SUJATA AGARWAL</h1>
+      
+      
+      <button onClick={()=>changeColor("red")}>RedColor</button>
+      <button onClick={()=>changeColor("blue")}>BlueColor</button>
+      <button onClick={()=>changeColor("darkgreen")}>GreenColor</button>
       
     </div>
   );
