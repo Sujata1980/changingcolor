d9aba48a0f809be593425e996150abfb6f580855
Sujata1980 <sujata.katoch@gmail.com>
2025-04-06 18:05:44 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import React,{useState} from 'react';
+import image from './assests/name.jpg'
 import './App.css';
 
 function App() {
@@ -16,18 +17,24 @@ function App() {
 
       <h1>Changes Color</h1>
       {/* <h1 style = {{ color : text ? "red" : " "}}>SUJATA AGARWAL</h1> */}
-
+      <img src={image} alt = "name" height ={200}/> 
       <h1 style = {{color}}>SUJATA AGARWAL</h1>
       
       
       <button onClick={()=>changeColor("red")} 
-      style = {{background:"red" , color:"white", borderRadius:"5px",margin:"10px",padding:"10px"}}>
+      style = {{background:"red" , color:"white",
+               borderRadius:"5px",margin:"10px",
+               padding:"5px", fontSize:"20px"}}>
       RedColor</button>
       <button onClick={()=>changeColor("blue")} 
-      style = {{background:"blue" , color:"white",borderRadius:"5px",margin:"10px",padding:"10px"}}>
+      style = {{background:"blue" , color:"white",
+              borderRadius:"5px",margin:"10px",
+              padding:"5px",fontSize:"20px"}}>
       BlueColor</button>
       <button onClick={()=>changeColor("darkgreen")} 
-      style = {{background:"green" , color:"white",borderRadius:"5px",margin:"10px",padding:"10px"}}>
+      style = {{background:"green" , color:"white",
+      borderRadius:"5px",margin:"10px",
+      padding:"5px",fontSize:"20px"}}>
       GreenColor</button>
       
     </div>
